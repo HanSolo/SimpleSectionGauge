b488b98eec2508bda68be21910aced3588d8d224
han.solo <han.solo@mac.com>
2016-07-25 10:12:10 +0200

    Initial code commit

---
+++ src/main/java/eu/hansolo/fx/simplesectiongauge/Demo.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.fx.simplesectiongauge;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+
+import java.util.Random;
+
+
+/**
+ * User: hansolo
+ * Date: 25.07.16
+ * Time: 08:33
+ */
+public class Demo extends Application {
+    private static final Random RND = new Random();
+    private SimpleSectionGauge gauge;
+    private long               lastTimerCall;
+    private AnimationTimer     timer;
+
+    @Override public void init() {
+        gauge = new SimpleSectionGauge();
+        lastTimerCall = System.nanoTime();
+        timer = new AnimationTimer() {
+            @Override public void handle(long now) {
+                if (now > lastTimerCall + 2_000_000_000) {
+                    gauge.setValue(RND.nextDouble() * 100);
+                    lastTimerCall = now;
+                }
+            }
+        };
+    }
+
+    @Override public void start(Stage stage) {
+        StackPane pane = new StackPane(gauge);
+        pane.setPadding(new Insets(20));
+
+        Scene scene = new Scene(pane);
+
+        stage.setTitle("SimpleSectionGauge");
+        stage.setScene(scene);
+        stage.show();
+
+        timer.start();
+    }
+
+    @Override public void stop() {
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
